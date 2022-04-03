55b2af032599403fe6ba30cf07a28c3bb6f40baf
Yuri Silva <44654440+WolfgangVing@users.noreply.github.com>
2022-04-03 00:01:21 -0300

    Adicionada validacao de regiao
    quando a lita de regiao é criada atraves da chamada callback, ela sera processada e filtrada para realizar doublecheck nas restricoes

--- challenge.js
+++ challenge.js
@@ -42,8 +42,9 @@ a este vendedor devem ser considerados inválidos.
 */
 
 /**
- *  pontos a lembrar,
- *  os códigos da loggi possuem apenas 15 dígitos, significa que qualquer codigoBarra com menos ou mais é inválidado.
+ *  Lembrar,
+ *  1. os códigos da loggi possuem apenas 15 dígitos, significa que qualquer codigoBarra com menos ou mais é inválidado.
+ *  Codigos de barra relacionado com 
  */
 const obj = {
 	pacote: 1,
@@ -58,7 +59,7 @@ const obj = {
 //Retrienving the data
 let data = require('./pacotes.json');
 
-function main(data, callback) {
+function main(data, callback, task) {
 
 	let dataPolished = []
 	
@@ -68,21 +69,24 @@ function main(data, callback) {
 
 		if(codigoBarra.length > 15 || codigoBarra.length < 15) {
 			return dataPolished.push({
-				pacote: index+1,
+				id: index+1,
 				codigoBarra: codigoBarra,
-				pacoteValido: false
+				pacoteValido: false,
+				message: "Codigo de barras inválido"
 			})
 		}
 		if(codigoBarra.includes("367")) {
 			return dataPolished.push({
-				pacote: index+1,
+				id: index+1,
 				codigoBarra: codigoBarra,
 				codigoVendedor: "367",
-				pacoteValido: false
+				pacoteValido: false,
+				message: "Este CNPJ está desativado."
 			})
 		}
+		
 		dataPolished.push({
-			pacote: index+1,
+			id: index+1,
 			codigoBarra: codigoBarra,
 			regiaoOrigem: codigoBarra.slice(0, 3),
 			regiaoDestino: codigoBarra.slice(3, 6),
@@ -93,15 +97,18 @@ function main(data, callback) {
 		})
 	});
 
-	//console.log(dataPolished)
-	callback(dataPolished)
+	//Essas constantes estão chamando a callback e armazenando o retorno da função
+	const regionsOrigem = callback(dataPolished, "regiaoOrigem")
+	const regionsDestino = callback(dataPolished, "regiaoDestino")
+	if(task === 1) console.log(dataPolished);
 }
 
+//validação do codigo de barras.
 
 /**
  *  Destiny Region Track
  *  
- *  função regiaoDestinos resolve desafio 1 e 4
+ *  função regiaoDestinoOrigem resolve desafio 1 e 4
  *  a cada iteração no array de data >
  *  está sendo filtrado os elementos por região >
  *  atribui esse elemento a sua região, este sendo um array definido no escopo da função com um objeto representado o total de pacotes >
@@ -113,24 +120,36 @@ function main(data, callback) {
 //Centro Oeste 201 ~ 299
 //Nordeste 300 ~ 399
 //Norte 400 ~ 499
-regiaoDestino = {
-	
-}
 
-function regiaoDestinos(data){
-	const sudeste = [{total: 0}];
+
+function regiaoDestinoOrigem(data, destinoOrOrigem){
+	let regioes = {}
+
+	const sudeste = [{total: 0}]
 	const sul = [{total: 0}]
 	const centroOeste = [{total: 0}]
 	const nordeste = [{total: 0}]
 	const norte = [{total: 0}]
 
 
-	data.forEach((element) => {
-		//se pacote for válido irá pular.
-		if(!element.pacoteValido) {
-			return
+	data.forEach((element, index) => {
+		const regionNum = parseInt(element[destinoOrOrigem])
+		
+		
+		// se o pacote for de origem centro oeste e o tipo do produto é joias atualizara para status invalido com mensagem.
+		if((destinoOrOrigem === "regiaoOrigem") && (regionNum >= 201 && regionNum <= 299) && (element["tipoPacote"] === "001")) {
+			data[index].pacoteValido = false
+			data[index].message = "Não transportamos joias de origem Centro-Oeste"
+			return 
 		}
-		const regionNum = parseInt(element["regiaoDestino"])
+
+
+		//se pacote for inválido irá pular.
+		if(!element.pacoteValido) {
+			return	
+		}
+		
+
 		if(regionNum >= 1 && regionNum <= 99) {
 			const total = sudeste.push(element) - 1
 			return sudeste[0].total = total
@@ -156,12 +175,17 @@ function regiaoDestinos(data){
 			return norte[0].total = total
 		}
 	})
-	regiaoDestino.sudeste = sudeste
-	regiaoDestino.sul = sul
-	regiaoDestino.centroOeste = centroOeste
-	regiaoDestino.nordeste = nordeste
-	regiaoDestino.norte = norte
-	console.log(regiaoDestino)
+	regioes.sudeste = sudeste
+	regioes.sul = sul
+	regioes.centroOeste = centroOeste
+	regioes.nordeste = nordeste
+	regioes.norte = norte
+
+	return regioes
 }
 
-main(data, regiaoDestinos)
+// Essa função irá listar os pacotes por região de Origem.
+function regiaoOrigem(data) {
+	
+}
+main(data, regiaoDestinoOrigem, 1)
